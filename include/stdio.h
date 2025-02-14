aacf60982ec2f6b68132836eb0c6950f970d4872
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-14 23:17:20 +0700

    Implement other unformatted output functions

--- include/stdio.h
+++ include/stdio.h
@@ -6,7 +6,21 @@ typedef struct {
   void *data; /* Empty struct is a GNU extension */
 } FILE;
 
+extern FILE *stdin;
+extern FILE *stdout;
+extern FILE *stderr;
+
 /* Unformatted I/O */
 
 /* Writes a character to a file stream */
 int fputc(int ch, FILE *stream);
+int putc(int ch, FILE *stream);
+
+/* Writes a character string to a file stream */
+int fputs(const char *str, FILE *stream);
+
+/* Writes a character to `stdout` */
+int putchar(int ch);
+
+/* Writes a character string to `stdout` */
+int puts(const char *str);
--- src/stdio.c
+++ src/stdio.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+FILE *stdin;
+FILE *stdout;
+FILE *stderr;
+
 __attribute__((weak)) int fputc(int ch, FILE *stream) {
   return EOF;
+}
+
+int putc(int ch, FILE *stream) {
+  return fputc(ch, stream);
+}
+
+int fputs(const char *str, FILE *stream) {
+  while (*str) {
+    if (fputc(*str++, stream) == EOF) {
+      return EOF;
+    }
+  }
+  return 0;
+}
+
+int putchar(int ch) {
+  return putc(ch, stdout);
+}
+
+int puts(const char *str) {
+  if (fputs(str, stdout) == EOF) {
+    return EOF;
+  }
+  return putc('\n', stdout);
 }
