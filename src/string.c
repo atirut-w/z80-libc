1e3832c2886e5db431ddae87dd14a36937f3af7f
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-16 22:39:57 +0700

    Hand-write memory manipulation functions
    This saves around 30 bytes or so

--- src/string.c
+++ src/string.c
@@ -8,19 +8,74 @@ size_t strlen(const char *str) {
   return len;
 }
 
-void *memset(void *dest, int ch, size_t count) {
-  unsigned char *d = dest;
-  while (count--) {
-    *d++ = ch;
-  }
-  return dest;
+__attribute__((naked)) void *memset(void *dest, int ch, size_t count) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    /* Load count and check for zero */
+    "ld c, (ix + 8)\n"
+    "ld b, (ix + 9)\n"
+    "ld a, b\n"
+    "or c\n"
+    "jr z, 1f\n"
+
+    /* Load other arguments */
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+    "ld a, (ix + 6)\n"
+
+    /* Setup */
+    "ld (hl), a\n"
+    "ld e, l\n"
+    "ld d, h\n"
+    "inc de\n"
+    "dec bc\n"
+    
+    /* Fill the rest */
+    "ldir\n"
+
+    /* Return a copy of the destination */
+    "1:\n"
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret"
+  );
 }
 
-void *memcpy(void *dest, const void *src, size_t count) {
-  unsigned char *d = dest;
-  const unsigned char *s = src;
-  while (count--) {
-    *d++ = *s++;
-  }
-  return dest;
+__attribute__((naked)) void *memcpy(void *dest, const void *src, size_t count) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    /* Load count and check for zero */
+    "ld c, (ix + 8)\n"
+    "ld b, (ix + 9)\n"
+    "ld a, b\n"
+    "or c\n"
+    "jr z, 1f\n"
+
+    /* Load other arguments */
+    "ld e, (ix + 4)\n"
+    "ld d, (ix + 5)\n"
+    "ld l, (ix + 6)\n"
+    "ld h, (ix + 7)\n"
+
+    /* Copy */
+    "ldir\n"
+
+    /* Return a copy of the destination */
+    "1:\n"
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret"
+  );
 }
