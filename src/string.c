be823aa206352b7aeec0bf77f7e39978e0fbd331
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-16 22:46:50 +0700

    Hand-write strlen
    This saves exactly four bytes.

--- src/string.c
+++ src/string.c
@@ -1,11 +1,31 @@
 #include <string.h>
 
-size_t strlen(const char *str) {
-  size_t len = 0;
-  while (*str++) {
-    len++;
-  }
-  return len;
+__attribute__((naked)) size_t strlen(const char *str) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+    "ld bc, 0\n"
+
+    "0:\n"
+    "ld a, (hl)\n"
+    "or a\n"
+    "jr z, 1f\n"
+    "inc hl\n"
+    "inc bc\n"
+    "jr 0b\n"
+
+    "1:\n"
+    "ld l, c\n"
+    "ld h, b\n"
+
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret\n"
+  );
 }
 
 __attribute__((naked)) void *memset(void *dest, int ch, size_t count) {
