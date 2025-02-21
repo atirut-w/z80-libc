e68c7c6fb712e1339d3e7c3d7351371076e46a13
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-21 09:43:40 +0700

    string: fix potential underflow bug

--- src/string.c
+++ src/string.c
@@ -8,23 +8,31 @@ char *strcpy(char *dest, const char *src) {
 
 char *strncpy(char *dest, const char *src, size_t count) {
   char *ret = dest;
-  while (count-- && (*dest++ = *src++));
-  while (count--) *dest++ = '\0';
+  size_t i;
+
+  for (i = 0; i < count && (*dest++ = *src++); i++);
+  for (; i < count; i++) *dest++ = '\0';
+
   return ret;
 }
 
 char *strcat(char *dest, const char *src) {
   char *ret = dest;
+
   while (*dest) dest++;
   while ((*dest++ = *src++));
+
   return ret;
 }
 
 char *strncat(char *dest, const char *src, size_t count) {
   char *ret = dest;
+  size_t i;
+
   while (*dest) dest++;
-  while (count-- && (*dest++ = *src++));
+  for (i = 0; i < count && (*dest++ = *src++); i++);
   *dest = '\0';
+  
   return ret;
 }
 
