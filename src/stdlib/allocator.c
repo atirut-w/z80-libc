2314ebd5c28c85551ae95df90be88b8e58f3ab0e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-22 21:36:27 +0700

    stdlib: split files to reduce footprint

--- src/stdlib/allocator.c
+++ src/stdlib/allocator.c
@@ -1,30 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ATEXIT 32
-
-static void (*atexit_funcs[MAX_ATEXIT])(void);
-static int atexit_count;
-
-void __exit(int status);
-
-void exit(int status) {
-  int i = 0;
-  
-  for (i = atexit_count - 1; i >= 0; i--) {
-    atexit_funcs[i]();
-  }
-  __exit(status);
-}
-
-int atexit(void (*func)(void)) {
-  if (atexit_count < MAX_ATEXIT) {
-    atexit_funcs[atexit_count++] = func;
-    return 0;
-  }
-  return 1;
-}
-
 /* Implementation for `malloc` and co. lovingly (?) taken from https://github.com/RAGUL1902/Dynamic-Memory-Allocation-in-C */
 
 typedef struct block {
