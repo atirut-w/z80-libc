b322861781b43884600acc4d296e9df1f88d838b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-23 16:17:31 +0700

    Implement more bits and math routines

--- src/runtime/bits.c
+++ src/runtime/bits.c
@@ -23,6 +23,35 @@ __attribute__((naked)) int _sshl(int a, int b) {
   );
 }
 
+__attribute__((naked)) long _lshl(long a, long b) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+    "ld e, (ix + 6)\n"
+    "ld d, (ix + 7)\n"
+    "ld b, (ix + 8)\n"
+
+    "0:\n"
+    "ld a, b\n"
+    "or a\n"
+    "jr z, 1f\n"
+    "sla l\n"
+    "rl h\n"
+    "rl e\n"
+    "rl d\n"
+    "djnz 0b\n"
+    "1:\n"
+
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret\n"
+  );
+}
+
 __attribute__((naked)) int _sshr(int a, int b) {
   __asm__(
     "push ix\n"
@@ -126,6 +155,39 @@ __attribute__((naked)) int _sand(int a, int b) {
   );
 }
 
+__attribute__((naked)) long _land(long a, long b) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+    "ld e, (ix + 6)\n"
+    "ld d, (ix + 7)\n"
+
+    "ld a, (ix + 8)\n"
+    "and l\n"
+    "ld l, a\n"
+
+    "ld a, (ix + 9)\n"
+    "and h\n"
+    "ld h, a\n"
+
+    "ld a, (ix + 10)\n"
+    "and e\n"
+    "ld e, a\n"
+
+    "ld a, (ix + 11)\n"
+    "and d\n"
+    "ld d, a\n"
+
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret\n"
+  );
+}
+
 __attribute__((naked)) int _sor(int a, int b) {
   __asm__(
     "push ix\n"
@@ -168,6 +230,35 @@ __attribute__((naked)) int _sxor(int a, int b) {
     "xor d\n"
     "ld h, a\n"
     
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret\n"
+  );
+}
+
+__attribute__((naked)) long _lshru(unsigned long a, unsigned int b) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+    "ld e, (ix + 6)\n"
+    "ld d, (ix + 7)\n"
+    "ld b, (ix + 8)\n"
+
+    "0:\n"
+    "ld a, b\n"
+    "or a\n"
+    "jr z, 1f\n"
+    "srl d\n"
+    "rr e\n"
+    "rr h\n"
+    "rr l\n"
+    "djnz 0b\n"
+    "1:\n"
+
     "ld sp, ix\n"
     "pop ix\n"
     "ret\n"
--- src/runtime/math.c
+++ src/runtime/math.c
@@ -1,4 +1,4 @@
-int _smulu(int a, int b) {
+int _smulu(unsigned int a, unsigned int b) {
   int sign, i;
   unsigned int ua, ub, prod;
 
@@ -10,6 +10,33 @@ int _smulu(int a, int b) {
   prod = 0;
 
   for (i = 0; i < 16; i++) {
+    if (ua & 1) {
+      prod += ub;
+    }
+
+    ua >>= 1;
+    ub <<= 1;
+  }
+
+  if (sign) {
+    prod = -prod;
+  }
+
+  return prod;
+}
+
+long _lmulu(unsigned long a, unsigned long b) {
+  int sign, i;
+  unsigned long ua, ub, prod;
+
+  sign = ((a ^ b) < 0);
+
+  ua = (a < 0) ? -a : a;
+  ub = (b < 0) ? -b : b;
+
+  prod = 0;
+
+  for (i = 0; i < 32; i++) {
     if (ua & 1) {
       prod += ub;
     }
@@ -87,6 +114,32 @@ int _srems(int a, int b) {
   return rem;
 }
 
+int _sremu(unsigned int a, unsigned int b) {
+  int i;
+  unsigned int ua, ub, quot, rem;
+
+  if (b == 0) {
+    return 0; /* TODO: Raise exception, somehow */
+  }
+
+  ua = a;
+  ub = b;
+
+  quot = 0;
+  rem = 0;
+
+  for (i = 15; i >= 0; i--) {
+    rem = (rem << 1) | ((ua >> i) & 1);
+
+    if (rem >= ub) {
+      rem -= ub;
+      quot |= (1 << i);
+    }
+  }
+
+  return rem;
+}
+
 __attribute__((naked)) int _sneg(int a) {
   __asm__(
     "push ix\n"
@@ -99,6 +152,39 @@ __attribute__((naked)) int _sneg(int a) {
 
     "or a\n"
     "sbc hl, de\n"
+
+    "ld sp, ix\n"
+    "pop ix\n"
+    "ret\n"
+  );
+}
+
+__attribute__((naked)) long _ladd(long a, long b) {
+  __asm__(
+    "push ix\n"
+    "ld ix, 0\n"
+    "add ix, sp\n"
+
+    "ld l, (ix + 4)\n"
+    "ld h, (ix + 5)\n"
+    "ld e, (ix + 6)\n"
+    "ld d, (ix + 7)\n"
+
+    "ld a, (ix + 8)\n"
+    "add a, l\n"
+    "ld l, a\n"
+
+    "ld a, (ix + 9)\n"
+    "adc a, h\n"
+    "ld h, a\n"
+
+    "ld a, (ix + 10)\n"
+    "adc a, e\n"
+    "ld e, a\n"
+
+    "ld a, (ix + 11)\n"
+    "adc a, d\n"
+    "ld d, a\n"
 
     "ld sp, ix\n"
     "pop ix\n"
