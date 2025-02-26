f0d327b3dacfb745799d92f02a6933e334639bbc
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-02-26 23:04:42 +0700

    Declare more integer types

--- include/stdint.h
+++ include/stdint.h
@@ -1,3 +1,11 @@
 #pragma once
 
+typedef signed char int8_t;
+typedef short int16_t;
+typedef long int32_t;
+
 typedef int intptr_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
