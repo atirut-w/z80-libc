ea701fb55f9afd666f271f298aee5e4ef312ec49
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-03-04 22:28:34 +0700

    Add `limits.h`

---
+++ include/limits.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/* Limits of integer types */
+#define CHAR_BIT 8
+
+#define SCHAR_MIN (-128)
+#define SCHAR_MAX 127
+#define UCHAR_MAX 255
+
+#define SHRT_MIN (-32768)
+#define SHRT_MAX 32767
+#define USHRT_MAX 65535
+
+#define INT_MIN (-32768)
+#define INT_MAX 32767
+#define UINT_MAX 65535
+
+#define LONG_MIN (-2147483648L)
+#define LONG_MAX 2147483647L
+#define ULONG_MAX 4294967295UL
